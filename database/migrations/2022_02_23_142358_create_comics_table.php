567f4b13021a64544339b4b41fc985d800264ab9
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-02-23 15:55:12 +0100

    first commit

---
+++ database/migrations/2022_02_23_142358_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('titolo', 20);
+            $table->string('editore', 60);
+            $table->text('trama');
+            $table->integer('numero');
+            $table->string('photo');
+            $table->date('data_uscita');
+            $table->float('prezzo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
