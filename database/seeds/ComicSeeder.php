8d3e8da8fd4a4b92a9b941bd9aca895567bec8b4
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-02-23 16:21:21 +0100

    add fake data on db

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Comic;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ComicSeeder extends Seeder
 {
@@ -9,8 +10,18 @@ class ComicSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 15; $i++) {
+        $newComic = new Comic();
+        $newComic->titolo = $faker->words(1, true);
+        $newComic->editore = $faker->words(1, true);
+        $newComic->trama = $faker->paragraphs(3, true);
+        $newComic->numero = $faker->randomNumber(5, true);
+        $newComic->photo = $faker->imageUrl(360, 360, 'animals', true, 'dogs', true);
+        $newComic->data_uscita = $faker->dateTime();
+        $newComic->prezzo = $faker->randomFloat(1, 20, 30);
+        $newComic->save();
+        }
     }
 }
